e0324dc70ecda528107763ba9509e69cf13d3ecd
ibrahemnaser <i.naser4040@gmail.com>
2022-08-04 18:14:52 +0200

    Modify Routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,25 @@
-import Cart from "./pages/Cart"
-import Home from "./pages/Home"
-import Login from "./pages/Login"
-import Product from "./pages/Product"
-import ProductList from "./pages/ProductList"
-import Regitster from "./pages/Regitster"
-import { Navigate, Route, Routes } from "react-router-dom"
+import Cart from "./pages/Cart";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Product from "./pages/Product";
+import ProductList from "./pages/ProductList";
+import Regitster from "./pages/Regitster";
+import { Navigate, Route, Routes } from "react-router-dom";
 
 const App = () => {
-	return (
-		<>
-			<Routes>
-				<Route path="/" element={<Navigate to="/home" />} />
-				<Route path="/home" element={<Home />} />
-				<Route path="/register" element={<Regitster />} />
-				<Route path="/login" element={<Login />} />
-				<Route path="/cart" element={<Cart />} />
-				<Route path="/products" element={<ProductList />} />
-				<Route path="/products/:id" element={<Product />} />
-			</Routes>
-		</>
-	)
-}
+  return (
+    <>
+      <Routes>
+        <Route path="/Furnite/" element={<Navigate to="/Furnite/home" />} />
+        <Route path="/Furnite/home" element={<Home />} />
+        <Route path="/Furnite/register" element={<Regitster />} />
+        <Route path="/Furnite/login" element={<Login />} />
+        <Route path="/Furnite/cart" element={<Cart />} />
+        <Route path="/Furnite/products" element={<ProductList />} />
+        <Route path="/Furnite/products/:id" element={<Product />} />
+      </Routes>
+    </>
+  );
+};
 
-export default App
+export default App;
